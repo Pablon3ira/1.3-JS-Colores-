14dc58370c424cfc74d33e3b4bf39f40a1594ea9
Pablo <juanpablo.neira@estudioenpenascal.com>
2024-03-12 10:35:01 +0100

    refactor: refactorize functions

--- main.js
+++ main.js
@@ -1,17 +1,19 @@
+function changeColor(from_color, to_color) {
+    const square = document.getElementById('square')
+    square.classList.remove(from_color)
+    square.classList.add(to_color)
+}
+
 function changeColorToBlue() {
-    const square = document.getElementById('square')
-    square.classList.remove('red')
-    square.classList.add('blue')
+     changeColor ('red','blue')
 }
 
-const button = document.getElementById('blue_button')
-blue_button.onclick = changeColorToBlue
-
-function changeColorToRed() {
-    const square = document.getElementById('square')
-    square.classList.remove('blue')
-    square.classList.add('red')   
+function changeColorToRed(){
+    changeColor('blue',('red'))
 }
+
+const blue_button = document.getElementById('blue_button')
+red_button.oncliclick = changeColorToBlue
 
 const red_button = document.getElementById('red_button')
 red_button.oncliclick = changeColorToRed
